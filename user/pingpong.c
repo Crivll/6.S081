a9e248716893f7767b4e78b56d886f4b358e49e4
Crivll <1225441967@qq.com>
2023-02-20 14:52:46 +0800

    lab 1: pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,7 +1,27 @@
-#include "user/user.h"
 #include "kernel/types.h"
+#include "user/user.h"
+#define MSGSIZE 5;
 
 int main()
 {
+  char *msg = "msg";
+  char buf[MSGSIZE];
+  int p[2];
+  pipe(p);
 
+  if (fork() > 0) {
+    write(p[1], msg, MSGSIZE);
+    while (read(p[0], buf, MSGSIZE) > 0) {
+      printf("pong recieved");
+      exit(0);
+    }
+    
+  }
+  else {
+    while (read(p[0], buf, MSGSIZE) > 0) {
+      printf("ping received");
+      write(p[1], msg, MSGSIZE);
+      exit(0);
+    }
+  }
 }
